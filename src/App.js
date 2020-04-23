5ce2810e4c79632815df0773f8611f927ac2bd8f
unknown <fitz4492@gmail.com>
2020-04-23 00:38:37 -0500

    updated eslintrc

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import { render } from 'react-dom'
-import Pet from './Pet';
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Missy" animal="Cat" breed="Orange/Black Mixed" />
+      <Pet name="Mochi" animal="Gecko" breed="Leopard" />
+      <Pet name="Rocket" animal="Dwarf Hamster" breed="Winter White" />
+    </div>
+  );
 };
 
 render(React.createElement(App), document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,11 @@
-import React from 'react';
-const Pet = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Luna"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Havanese"),
-  ]);
+import React from "react";
+const Pet = ({ name, animal, breed }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 };
-export default Pet
+export default Pet;
