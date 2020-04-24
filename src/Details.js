9e88b42d8389576f94e6dce53c955a7f45b69e33
unknown <fitz4492@gmail.com>
2020-04-23 22:15:59 -0500

    Context set up globally to manage state of color theme

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,8 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
 
 class Details extends React.Component {
   state = { loading: true };
@@ -30,7 +32,12 @@ class Details extends React.Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
+          {/* This how class based components must use context */}
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
       </div>
@@ -47,4 +54,10 @@ class Details extends React.Component {
 //   );
 // };
 
-export default Details;
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
---
+++ src/ErrorBoundary.js
@@ -0,0 +1,36 @@
+// mostly code from react.js docs
+import React from "react";
+import { Link, Redirect } from "@reach/router";
+
+class ErrorBoundary extends React.Component {
+  state = { hasError: false, redirect: false };
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary caught and error: ", error, info);
+  }
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(() => this.setState({ redirect: true }), 5000);
+    }
+  }
+
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    }
+    if (this.state.hasError) {
+      return (
+        <h1>
+          There was an error with this listing. <Link to="/">Click here</Link>{" "}
+          to go back to the home page or wait five seconds.
+        </h1>
+      );
+    }
+    // eslint-disable-next-line
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
