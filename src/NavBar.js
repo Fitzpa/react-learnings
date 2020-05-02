fb30bef833afbf429416b833e4efd6811d8b9fa3
unknown <fitz4492@gmail.com>
2020-05-02 17:30:52 -0500

    basics of emotion

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,19 +1,37 @@
-import React from "react";
-import { Link } from "reach/router";
+import React, { useState } from "react";
+import { Link } from "@reach/router";
 import { css } from "@emotion/core";
+import colors from "./colors";
 
-const NavBar = () => (
-  <header
-    css={css`
-      background-color: #333;
-      padding: 15px;
-    `}
-  >
-    <Link to="/">Adopt Me!</Link>
-    <span aria-label="logo" role="img">
-      🦝
-    </span>
-  </header>
-);
+const NavBar = () => {
+  const [padding, setPadding] = useState(15);
+  return (
+    // eslint-disable-next-line
+    <header
+      onClick={() => setPadding(padding + 15)}
+      css={css`
+        background-color: ${colors.primary};
+        padding: ${padding}px;
+      `}
+    >
+      <Link to="/">Adopt Me!</Link>
+      <span
+        css={css`
+          font-size: 60px;
+          cursor: pointer;
+          transition: all 0.3s ease;
+          border-bottom: 3px solid transparent;
+          &:hover {
+            border-bottom: 3px solid ${colors.secondary};
+          }
+        `}
+        aria-label="logo"
+        role="img"
+      >
+        🦝
+      </span>
+    </header>
+  );
+};
 
 export default NavBar;
