b03aa84f966a09f198301b0988f625fa1e3817b2
unknown <fitz4492@gmail.com>
2020-05-02 23:28:13 -0500

    server-side rendering using renderToNodeStream. This method is preferable and more efficient.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import React from "react";
-import { renderToString } from "react-dom/server";
+import { renderToNodeStream } from "react-dom/server";
 import { ServerLocation } from "@reach/router";
 import fs from "fs";
 import App from "../src/App";
@@ -23,6 +23,10 @@ app.use("/dist", express.static("dist"));
 // This is the core part of how to get this to work with React
 // You rendered on the server and then you send it down to the client as complete markup
 app.use((req, res) => {
+  // parts[0] is the first half of the markup
+  // res.write(parts[0]) is saying, here is all the CSS,
+  // go download this while I render JavaScript in the background
+  res.write(parts[0]);
   const reactMarkup = (
     // This is from Reach/Router to do the server-side rendering
     <ServerLocation url={req.url}>
@@ -30,10 +34,21 @@ app.use((req, res) => {
     </ServerLocation>
   );
 
-  //   first part of the html, rendered markup, second part of the html
-  res.send(parts[0] + renderToString(reactMarkup) + parts[1]);
+  // Using stream will progressively render the app one piece at a time
+  const stream = renderToNodeStream(reactMarkup);
 
-  res.end();
+  // This is saying connect the pipes
+  // send the respones to the user
+  // but dont end
+  stream.pipe(res, { end: false });
+
+  // This is saying, once finished getting the response
+  // get the second half of the markup and end the connection
+  stream.on("end", () => {
+    // parts[1] is the second half of the markup.
+    res.write(parts[1]);
+    res.end();
+  });
 });
 
 // eslint-disable-next-line
