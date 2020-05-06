c791605720def0c05653d97ef7fcd34eced5d622
unknown <fitz4492@gmail.com>
2020-05-05 22:55:34 -0500

    set up a basic mock with jest

---
+++ __mocks__/@frontendmasters/pet.js
@@ -0,0 +1,35 @@
+import { readFileSync } from "fs";
+import path from "path";
+import { act } from "@testing-library/react";
+
+const breeds = [
+  { name: "Bichon Frise" },
+  { name: "Bolognese" },
+  { name: "Coton de Tulear" },
+  { name: "Havanese" },
+  { name: "Maltese" },
+];
+
+const doggos = JSON.parse(
+  readFileSync(path.join(__dirname, "res.json")).toString()
+);
+
+export const ANIMALS = ["dog", "cat", "bird"];
+// The underscore is just to signify that this is only to be used for testing
+export const _breeds = breeds;
+export const _dogs = doggos.animals;
+
+const mock = {
+  breeds: jest.fn(() => {
+    return {
+      then: (callback) => act(() => callback({ breeds })),
+    };
+  }),
+  animals: jest.fn(() => {
+    return {
+      then: (callback) => act(() => callback(doggos)),
+    };
+  }),
+};
+
+export default mock;
